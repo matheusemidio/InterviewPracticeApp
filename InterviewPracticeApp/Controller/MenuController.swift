15c8e391892aa35a62b275114ff5f33f315c70c4
Matheus Emidio de Melo Cadena <matheus8emidio@gmail.com>
2022-02-16 16:38:43 -0500

    Created the views

---
+++ InterviewPracticeApp/Controller/MenuController.swift
@@ -0,0 +1,20 @@
+//
+//  MenuController.swift
+//  InterviewPracticeApp
+//
+//  Created by Matheus Cadena on 2022-02-16.
+//
+
+import UIKit
+
+class MenuController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ InterviewPracticeApp/Controller/PracticeController.swift
@@ -0,0 +1,20 @@
+//
+//  PracticeController.swift
+//  InterviewPracticeApp
+//
+//  Created by Matheus Cadena on 2022-02-16.
+//
+
+import UIKit
+
+class PracticeController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
